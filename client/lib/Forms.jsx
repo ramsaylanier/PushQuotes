92e9e3662664f218a132bc4df1876b9d46093feb
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-01 16:28:19 -0400

    Better handling of hashtags in new deck - no more hashtags with spaces or duplicates

--- client/lib/Forms.jsx
+++ client/lib/Forms.jsx
@@ -38,7 +38,7 @@ newDeckForm = {
 		var deckAttributes = {
 			title: $(e.currentTarget).find('[name=deck-title-field]').val(),
 			slug: $(e.currentTarget).find('[name=deck-slug-field]').val(),
-			hashtags: $(e.currentTarget).find('[name=deck-hashtags-field]').val().split(','),
+			hashtags: $(e.currentTarget).find('[name=deck-hashtags-field]').val().split(/,| /g),
 			withSlides: $(e.currentTarget).find('[name=use-slides-field]').get(0).checked,
 			author: Meteor.userId()
 		};
@@ -46,6 +46,13 @@ newDeckForm = {
 		_.each(deckAttributes.hashtags, function(hashtag, index){
 			deckAttributes.hashtags[index] = hashtag.trim();
 		})
+
+		deckAttributes.hashtags = _.filter(deckAttributes.hashtags, function(e){
+			return e.length > 0
+		})
+
+		deckAttributes.hashtags = _.uniq(deckAttributes.hashtags)
+
 		
 		Meteor.call('createDeck', deckAttributes, function(error,result){
 			if (error){
