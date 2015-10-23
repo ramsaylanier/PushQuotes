540f2ab3268e4ef9c4fc951bc2a5b60c33868b98
Ramsay Lanier <ramsay@nclud.com>
2015-10-23 00:58:40 -0400

    fixed register page render error

--- client/views/Pages/registerPage.jsx
+++ client/views/Pages/registerPage.jsx
@@ -1,10 +1,12 @@
 RegisterPage = React.createClass({
 	render(){
 		return(
-			<Page animateIn={DefaultPageAnimateIn} backgroundImage={'/img/login-bg.jpg'}>
-				<div className="wrapper form-wrapper white-bg">
-					<Form attributes={registerFormAttributes} />
-				</div>
+			<Page backgroundImage={'/img/login-bg.jpg'}>
+				<PageContent>
+					<div className="wrapper form-wrapper white-bg">
+						<Form attributes={registerFormAttributes} />
+					</div>
+				</PageContent>
 			</Page>
 		)
 	}
